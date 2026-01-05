5e3bf0715e6b8dbaf600ab80184b25a4a37209bf
mllamasboxel <manuel.llamas@boxelstudio.com>
2026-01-05 11:10:29 -0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -10,3 +10,9 @@ menu.addCommand(
 
 import GrayAutoBackdrop
 nuke.menu('Nuke').addCommand('Extra/Gray Auto Backdrop', lambda: GrayAutoBackdrop.GrayAutoBackdrop(), "alt+b", shortcutContext=2)
+
+
+
+nuke.knobDefault("Blur.label", "Size: [value size]")
+nuke.knobDefault("Merge2.label", "Op: [value operation]")
+nuke.knobDefault("Multiply.label", "Val: [value value]")
